800aa7adfdf44aedcf18facd660f0dcbc082c905
trikitrok <trikitrok@gmail.com>
2016-01-21 07:55:00 +0100

    Separated  the responsibility of computing the factor applied to the postage weight

--- src/let_gone_wild_refactoring/core.clj
+++ src/let_gone_wild_refactoring/core.clj
@@ -1,12 +1,15 @@
 (ns let-gone-wild-refactoring.core)
 
-(defn postage-cost [{:keys [dimensions weight days-to-ship]}]
+(defn factor [{:keys [dimensions weight days-to-ship]}]
   (let [{height :height width :width} dimensions
         big? (and (> height 10) (> width 10))
         express? (> 2 days-to-ship)
-        multiple (cond
-                   (and express? big?) 5
-                   express? 3
-                   big? 2
-                   :else 1)]
-    (* multiple weight)))
+        factor (cond
+                 (and express? big?) 5
+                 express? 3
+                 big? 2
+                 :else 1)]
+    factor))
+
+(defn postage-cost [postage]
+  (* (factor postage) (:weight postage)))
