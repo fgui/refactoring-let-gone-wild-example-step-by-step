8cf8aacc3dd87bb0b619a57927463908710d468d
trikitrok <trikitrok@gmail.com>
2016-01-21 08:12:58 +0100

    Moved postage classification to its own name space

--- src/let_gone_wild_refactoring/core.clj
+++ src/let_gone_wild_refactoring/core.clj
@@ -1,37 +1,6 @@
-(ns let-gone-wild-refactoring.core)
-
-(defn big? [{{:keys [height width]} :dimensions}]
-  (and (> height 10) (> width 10)))
-
-(defn express? [{:keys [days-to-ship]}]
-  (> 2 days-to-ship))
-
-(defn big-and-express [postage]
-  (assoc postage :type :big-and-express))
-
-(defn express [postage]
-  (assoc postage :type :express))
-
-(defn big [postage]
-  (assoc postage :type :big))
-
-(defn regular [postage]
-  (assoc postage :type :regular))
-
-(defn classify-postage [postage]
-  (cond
-    (and (express? postage)
-         (big? postage))
-    (big-and-express postage)
-
-    (express? postage)
-    (express postage)
-
-    (big? postage)
-    (big postage)
-
-    :else
-    (regular postage)))
+(ns let-gone-wild-refactoring.core
+  (:require
+    [let-gone-wild-refactoring.postage-classification :as postage-classification]))
 
 (def factors-by-postage-type
   {:big-and-express 5
@@ -41,7 +10,7 @@
 
 (defn factor [postage]
   (-> postage
-      classify-postage
+      postage-classification/classify-postage
       :type
       factors-by-postage-type))
 
--- src/let_gone_wild_refactoring/postage_classification.clj
+++ src/let_gone_wild_refactoring/postage_classification.clj
@@ -1,1 +1,34 @@
 (ns let-gone-wild-refactoring.postage-classification)
+
+(defn- big? [{{:keys [height width]} :dimensions}]
+  (and (> height 10) (> width 10)))
+
+(defn- express? [{:keys [days-to-ship]}]
+  (> 2 days-to-ship))
+
+(defn- big-and-express [postage]
+  (assoc postage :type :big-and-express))
+
+(defn- express [postage]
+  (assoc postage :type :express))
+
+(defn- big [postage]
+  (assoc postage :type :big))
+
+(defn- regular [postage]
+  (assoc postage :type :regular))
+
+(defn classify-postage [postage]
+  (cond
+    (and (express? postage)
+         (big? postage))
+    (big-and-express postage)
+
+    (express? postage)
+    (express postage)
+
+    (big? postage)
+    (big postage)
+
+    :else
+    (regular postage)))
