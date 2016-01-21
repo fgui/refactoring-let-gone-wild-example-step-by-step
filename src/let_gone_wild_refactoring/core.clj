2922eff044ee5fdb3c57e297546c372cff15cad4
trikitrok <trikitrok@gmail.com>
2016-01-21 08:02:07 +0100

    Separating postage classification from assignation of factors to each kind of postage - Big and express postage done

--- src/let_gone_wild_refactoring/core.clj
+++ src/let_gone_wild_refactoring/core.clj
@@ -6,12 +6,24 @@
 (defn express? [{:keys [days-to-ship]}]
   (> 2 days-to-ship))
 
+(defn big-and-express [postage]
+  (assoc postage :type :big-and-express))
+
+(defn classify-postage [postage]
+  (when (and (express? postage) (big? postage))
+    (big-and-express postage)))
+
+(def factors-by-postage-type
+  {:big-and-express 5})
+
 (defn factor [postage]
-  (cond
-    (and (express? postage) (big? postage)) 5
-    (express? postage) 3
-    (big? postage) 2
-    :else 1))
+  (if-let [factor (factors-by-postage-type (:type (classify-postage postage)))]
+    factor
+    (cond
+      (and (express? postage) (big? postage)) (throw (Exception. "should't get here!"))
+      (express? postage) 3
+      (big? postage) 2
+      :else 1)))
 
 (defn postage-cost [postage]
   (* (factor postage) (:weight postage)))
