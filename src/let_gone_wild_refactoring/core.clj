b25036e7b452e9320adaee6db39ec4b2820c99a4
trikitrok <trikitrok@gmail.com>
2016-01-21 07:57:41 +0100

    Extracted helper to know when a postage is express

--- src/let_gone_wild_refactoring/core.clj
+++ src/let_gone_wild_refactoring/core.clj
@@ -3,14 +3,15 @@
 (defn big? [{{:keys [height width]} :dimensions}]
   (and (> height 10) (> width 10)))
 
-(defn factor [{:keys [days-to-ship] :as postage}]
-  (let [express? (> 2 days-to-ship)
-        factor (cond
-                 (and express? (big? postage)) 5
-                 express? 3
-                 (big? postage) 2
-                 :else 1)]
-    factor))
+(defn express? [{:keys [days-to-ship]}]
+  (> 2 days-to-ship))
+
+(defn factor [postage]
+  (cond
+    (and (express? postage) (big? postage)) 5
+    (express? postage) 3
+    (big? postage) 2
+    :else 1))
 
 (defn postage-cost [postage]
   (* (factor postage) (:weight postage)))
